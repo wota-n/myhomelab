303b5e057cb4775c96f62472d80df6dfce7a55aa
wota-n <najmeenajib99@gmail.com>
2024-02-19 18:52:27 +0800

    pihole lxc done for now. failed to automate this

--- pihole/README.md
+++ pihole/README.md
@@ -1,2 +1,4 @@
-# myhomelab
-repo to document services and apps i add to my homelab
+# Pihole with Cloudflared
+This folder is to document the install of Pihole with Cloudflared using an LXC in Proxmox. Image used is Debian 12.
+
+Probably better to just use the docker image for most people
--- pihole/init-pihole.sh
+++ pihole/init-pihole.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
-apt install curl sudo
-curl -sSL https://install.pi-hole.net | bash
+apt-get update && apt-get upgrade -y
+systemctl reload postfix
+apt-get install curl sudo git vim net-tools -y
+curl -sSL https://install.pi-hole.net | sudo PIHOLE_SKIP_OS_CHECK=true bash
+# Cloudflared Install
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+sudo apt-get install ./cloudflared-linux-amd64.deb
+cloudflared -v
+sudo useradd -s /usr/sbin/nologin -r -M cloudflared
+# commented out because this can't be done on script for whatever reason
+# https://docs.pi-hole.net/guides/dns/cloudflared/?h=cloud
+# echo CLOUDFLARED_OPTS=--port 5053 --upstream https://1.1.1.1/dns-query --upstream https://1.0.0.1/dns-query > /etc/default/cloudflared
+# sudo chown cloudflared:cloudflared /etc/default/cloudflared
+# sudo chown cloudflared:cloudflared /usr/local/bin/cloudflared
+# source /etc/default/cloudflared
+# cat > /etc/systemd/system/cloudflared.service << EOF
+# [Unit]
+# Description=cloudflared DNS over HTTPS proxy
+# After=syslog.target network-online.target
+
+# [Service]
+# Type=simple
+# User=cloudflared
+# EnvironmentFile=/etc/default/cloudflared
+# ExecStart=/usr/local/bin/cloudflared proxy-dns --port 5053 $CLOUDFLARED_OPTS
+# Restart=on-failure
+# RestartSec=10
+# KillMode=process
+
+# [Install]
+# WantedBy=multi-user.target
+# EOF
+# sudo systemctl enable cloudflared
+# sudo systemctl start cloudflared
+# sudo systemctl restart cloudflared
+# apt remove curl git net-tools -y
